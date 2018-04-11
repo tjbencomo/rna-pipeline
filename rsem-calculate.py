6e9e743f453af5e28546991bad8928c7cbdb49d0
Tomas Bencomo <tjbencomo@gmail.com>
2018-04-11 15:48:03 -0700

    Initial code RSEM module. rsem.sh works, have not tested rsem-calculate.py wrapper yet

---
+++ rsem-calculate.py
@@ -0,0 +1,68 @@
+'''
+Author: Tomas Bencomo
+
+Script to execute the RSEM software package to quantify rna expression. Acts as a wrapper for the rsem.sh bash script. Submits sbatch call for rsem.sh with SBATCH directives for --workdir, --output, and --error.
+
+This script:
+Parses command line options
+submits sbatch job for rsem.sh script
+
+Command line parameters:
+input bam file
+RSEM reference directory
+working directory
+
+Determines based off command line parameters:
+Sample prefix name based on input file
+
+'''
+
+import argparse
+import subprocess
+import os
+
+parser = argparse.ArgumentParser(description='Launch rsem-calculate-expression')
+
+parser.add_argument('-I', '--input-bam-file', metavar='--input-bam-file', type=str, nargs=1, help='Aligned transcriptome bam file to quanitify reads', dest='input_bam')
+
+parser.add_argument('-rDir', '--rsem-ref-directory', metavar='-rsem-ref-directory', type=str, nargs=1, help='location of reference files rsem needs to operate', dest='ref_directory')
+
+parser.add_argument('-wd', '--working-directory', metavar='--working-directory', type=str, nargs=1, help='location to store output files', dest='working_directory')
+
+args = parser.parse_args()
+
+
+#Fix formating for parameters after parsing to strings
+args.working_directory = ''.join(args.working_directory)
+args.input_bam = ''.join(args.input_bam)
+args.ref_directory = ''.join(args.ref_directory)
+
+file_name = args.input_bam
+
+while '/' in file_name:
+	file_name = file_name[file_name.find('/')+1:]
+
+file_prefix = file_name[0:file_name.find('Aligned')]
+
+#print args.working_directory
+#print args.input_bam
+#print args.ref_directory
+#print file_prefix
+
+USER = os.environ['USER']
+
+workdir = "--workdir=" + args.working_directory
+output = "--output=/home/users/" + USER + "/out/rsem.%j.out"
+error = "--error=/home/users/" + USER + "/errout/rsem.%j.err"
+mail = "--mail-user" + USER + "@stanford.edu" #assumes user's email is structured sherlock_username@stanford.edu
+
+subprocess.call(['sbatch', workdir, output, error, mail, 'star.sh', '-b', args.input_bam, '-prefix', file_prefix, '-rDir', args.ref_directory])
+
+
+
+
+
+
+
+
+
---
+++ rsem.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --job-name=rsem-internal
+#SBATCH --nodes=1
+#SBATCH --mem=10000
+#SBATCH --time=0-12:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=tbencomo@stanford.edu
+#SBATCH --workdir=/scratch/users/tbencomo/RNA_seq/pipeline-tests
+#SBATCH --output=/home/users/tbencomo/out/rsem.%j.out
+#SBATCH --error=/home/users/tbencomo/errout/rsem.%j.err
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -b|--aligned-transcriptome-bam)
+    BAM="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -prefix|--filePrefixName)
+    PREFIX="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -rDir|--referenceDirectory)
+    REF_DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo BAM        = "${BAM}"
+echo PREFIX	= "${PREFIX}"
+echo REF_DIR	= "${REF_DIR}"
+echo DEFAULT         = "${DEFAULT}"
+
+rsem-calculate-expression --alignments --paired-end --ci-memory 4000 --num-threads $SLURM_CPUS_ON_NODE $BAM $REF_DIR $PREFIX
+
