3bcdbe801be0a45979b09b420a3a97a31dc1eedc
Tomas Bencomo <tjbencomo@gmail.com>
2018-05-24 14:34:08 -0700

    Add launchpipeline method

--- pipeline.py
+++ pipeline.py
@@ -18,22 +18,105 @@ import subprocess
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description = 'Launch complete pipeline')
-parser.add_argument('-I', '--inputDirectory', metavar='--inputDirectory', type = str, nargs = 1, help = 'Directory containing FASTQ files to process', dest='directory')
-
-args = parser.parse_args()
-
-directory = ''.join(args.directory)
-
-if directory == None:
-	print "No directory specified. Exiting"
-	sys.exit()
+#parseArgs
+#getSamplePairs
+#launchPipeline
+#main
 
 
-files = os.listdir(directory)
-files = [file for file in files if '.fastq.gz' in file]
+def findParentDirectory(directory):
+	if directory[len(directory)-1] == '/':
+		splitter = directory.rfind('/', 0, len(directory) - 1)
+	else:
+		splitter = directory.rfind('/', 0, len(directory))
+	parent_directory = directory[0:splitter] + '/'
+	
+	return parent_directory
 
-#Next - compute sample pairs by finding string between the _ and .fastq pattern
-#Add the additional command line arguments for the other pipeline steps stuch as the star reference directory, rsem reference directory and other stuff
+def extractDirectoryName(directory):
+	if directory[len(directory)-1] == '/':
+		splitter = directory.rfind('/', 0, len(directory) - 1)
+		directoryName = directory[splitter+1 : len(directory)-1]
+	else:
+		splitter = directory.rfind('/', 0, len(directory))
+		directoryName = directory[splitter+1: ]
+	return directoryName
+
+def parseArgs():
+	parser = argparse.ArgumentParser(description = 'Launch complete pipeline')
+	parser.add_argument('-I', '--inputDirectory', metavar='--inputDirectory', type = str, nargs = 1, 
+				help = 'Directory containing FASTQ files to process', dest='directory')
+	parser.add_argument('-gDir', '--genomeDirectory', metavar='--genomeDirectory', type=str, nargs=1, 
+				help = 'Directory containing reference genome files for STAR', dest= 'genomeDirectory')
+	parser.add_argument('-rDir', '--rsemReferenceDirectory', metavar='--rsemReferenceDirectory', type=str, nargs=1, 
+				help = 'Directory containing RSEM reference files', dest='rsemDirectory')
+	parser.add_argument('-output', '--outputDirectory', metavar='--outputDirectory', type=str, nargs=1, dest='outputDirectory')
+
+	args = parser.parse_args()
+
+	directory = ''.join(args.directory)
+	genomeDirectory = ''.join(args.genomeDirectory)
+	rsemDirectory = ''.join(args.rsemDirectory)
+
+	if args.outputDirectory == None:
+		outputDirectory = findParentDirectory(''.join(args.directory)) + extractDirectoryName(directory) + '-output'
+	else:
+		outputDirectory = ''.join(args.outputDirectory)
+
+
+	return (directory, genomeDirectory, rsemDirectory, outputDirectory) 
+
+
+def getSamplePairs(directory):
+	if directory == None:
+		print "No directory specified. Exiting"
+		sys.exit(1)
+
+	files = os.listdir(directory)
+	files = [file for file in files if '.fastq.gz' in file]
+	
+	samples = {}
+
+	for file in files:
+		file_extension_pos = file.find('.fastq.gz')
+		paired_end_indicator_pos = file.rfind('_', 0, file_extension_pos)
+		pair_version = file[paired_end_indicator_pos:file_extension_pos]
+		prefix = file[0:paired_end_indicator_pos]
+		
+		if prefix in samples:
+			samples[prefix].append(file)
+		else:
+			samples[prefix] = [file]
+	
+	for sample in samples:
+		if len(samples[sample]) != 2:
+			print("Not a pair of files. Either too less or too many files to be considered a pair. Something went wrong with pair processing. Sample " + prefix + " has been removed from analysis")
+			samples.pop(sample, None)
+
+	return samples
+
+
+def launchPipeline(genome_directory, rsem_directory, output_directory, samples):
+	PIPE_DIR = os.getcwd()
+	for sample in samples:
+		paired_files = samples[sample]
+		output = subprocess.check_output(['python', PIPE_DIR + '/star-aligner.py', '-wd', output_directory, 
+						'-f1', paired_files[0], '-f2', paired_files[1], '-gDir', genome_directory])
+		phrase = 'Submitted batch job'
+		index = output.find(phrase)
+		end = index + len(phrase)
+		jobID = output[end + 1:output.find('\n')]
+
+def main():
+	directory, genomeDirectory, rsemDirectory, outputDirectory = parseArgs()
+	samples = getSamplePairs(directory)
+	launchPipeline(genomeDirectory, rsemDirectory, outputDirectory, samples)
+	print("All jobs launched")
+
+
+if __name__ == "__main__": main()
+
+
+# finish creating a directory-pipeline-output option if no output directory inputted
 # write launch steps
 # Turn all of these steps into functions
