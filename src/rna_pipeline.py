795a0d5a8e6d8509ea4175275b14facc28651bee
Tomas Bencomo <tjbencomo@gmail.com>
2018-11-09 14:58:44 -0800

    Change file naming convention. Add functionality to rna_pipeline and star_aligner

--- src/rna_pipeline.py
+++ src/rna_pipeline.py
@@ -32,22 +32,22 @@ import difflib
 def createParser():
     parser = argparse.ArgumentParser(description='Pipeline to process RNA-Seq data')
     parser.add_argument('-I', '--input', type=str, nargs='*', 
-                        help='Specify input directory or input files.If specifying files, only 1 pair of files can be specified.',
+                        help='Specify input directory or input files.',
                         dest='input')
     parser.add_argument('-gDir', '--genomeDirectory', type=str, nargs=1, 
-                        help='Optional. Location of reference directory for STAR. If not used, uses directory from config.ini',
+                        help='Optional. Location of reference directory for STAR. If not used, uses directory from rna-pipeline-config.ini',
                         dest='genomeDirectory')
     parser.add_argument('-rDir', '--rsemDirectory', type=str, nargs=1,
-                        help='Optional. Location of reference directory for RSEM. If not used, uses directory from config.ini',
+                        help='Optional. Location of reference directory for RSEM. If not used, uses directory from rna-pipeline-config.ini',
                         dest='rsemDirectory')
     parser.add_argument('-countRef', '--htseqCountReferenceFile', type=str, nargs=1,
-                        help='Optional. GTF file path for HTSEQ-Count. If not used, uses directory from config.ini',
+                        help='Optional. GTF file path for HTSEQ-Count. If not used, uses directory from rna-pipeline-config.ini',
                         dest='htseqReferenceFile')
     parser.add_argument('-outLog', '--outputLogDirectory', type=str, nargs=1,
-                        help='Optional. Location to store output log file. If not used, uses directory from config.ini',
+                        help='Optional. Location to store output log file. If not used, uses directory from rna-pipeline-config.ini',
                         dest='outputLogDirectory')
     parser.add_argument('-errorLog', '--errorLogDirectory', type=str, nargs=1, 
-                        help='Optional. Location to store error log file. If not used, uses directory from config.ini',
+                        help='Optional. Location to store error log file. If not used, uses directory from rna-pipeline-config.ini',
                         dest='errorLogDirectory')
     parser.add_argument('-DESeq', '--includeDESeqAnalysis', type=bool, nargs=1,
                         help='Optional. Boolean value whether or not to run differential expression analysis. Defaults to True.',
@@ -66,8 +66,12 @@ def parseArgs():
 
     # Argument handling - check input and fix if needed
     if len(args.input) == 1:
-        pipelineInput = args.input[0]
-        inputIsDirectory = True
+        pipelineInput = args.input
+        if os.path.isdir(pipelineInput) is True:
+            inputIsDirectory = True
+        else:
+            inputIsDirectory = False
+            includeDESeq = False
     elif len(args.input) == 2:
         pipelineInput = args.input
         inputIsDirectory = False
@@ -80,7 +84,6 @@ def parseArgs():
     else:
         outputDirectory = args.outputDirectory[0]
     
-
     configurationSettings = loadConfigData()
 
     if args.genomeDirectory is None:
@@ -124,7 +127,7 @@ def loadConfigData():
     userDirectory = os.environ['HOME']
 
     config = configparser.ConfigParser()
-    config.read(os.path.join(userDirectory, 'config.ini'))
+    config.read(os.path.join(userDirectory, 'rna-pipeline-config.ini'))
 
     return config
 
@@ -135,7 +138,7 @@ def getSamplePairs(directory):
         files.remove(f)
         match = difflib.get_close_matches(f, files, n=1)[0]
         print(match)
-        samples.append((f, match))
+        samples.append((os.path.join(directory, f), os.path.join(directory, match)))
         files.remove(match)
     
     return samples
